68e9f3e538d1a2dda5f4aab155a6de230a083517
Mikhedov Konstantin <mihedovkos@gmail.com>
2022-03-04 20:39:46 +0300

    Add servers start point

--- source/app.d
+++ source/app.d
@@ -1,2 +1,12 @@
-module app;
+/**
+ * Server for the μStep Andropid app
+ * Author: KonstantIMP <mihedovkos@gmail.com>
+ * Date: 4 March 2022
+ */
+module org.kimp.mu.step;
 
+int main(string [] args)
+{
+
+    return 0;
+}
